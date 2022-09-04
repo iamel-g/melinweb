4741c1d5769b40a3dc6257149be106c6bb8f8cb0
m_galvan <mgalvan@outlook.fr>
2022-09-05 01:25:01 +0200

    Add gitlab link

--- wp-content/themes/melinweb/footer.php
+++ wp-content/themes/melinweb/footer.php
@@ -1,30 +1,36 @@
 </div><!-- #content -->
 
-<footer id="colophon" class="site-footer bg-dark text-light" role="contentinfo">
-	<ul class="nav row col-md-4 mx-auto text-center mt-4">
-		<li class="col-md-6 col-lg-4">
-			<a href="https://linkedin.com/in/mgvn/" target="_blank"><i class="fa-brands fa-linkedin fa-3x text-light"></i></a>
-			<p>in/mgvn</p>
-		</li>
-		<li class="col-md-6 col-lg-4">
-			<a href="https://twitter.com/iamel_g" target="_blank"><i class="fa-brands fa-twitter-square fa-3x text-light"></i></a>
-			<p>@iamel_g</p>
-		</li>
-		<li class="col-md-6 col-lg-4">
-			<a href="https://hackmd.io/@mgalvan" target="_blank"><i class="fa-solid fa-file-lines fa-3x text-light"></i></a>
-			<p>@mgalvan</p>
-		</li>
-	</ul>
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="col-lg-2 mx-auto">
+		<ul class="row m-0 justify-content-evenly text-center pt-4">
+			<li class="col-4 col-lg-4">
+				<a href="https://linkedin.com/in/mgvn/" target="_blank"><i class="fa-brands fa-linkedin fa-2x"></i></a>
+				<p>in/mgvn</p>
+			</li>
+			<li class="col-4 col-lg-4">
+				<a href="https://twitter.com/iamel_g" target="_blank"><i class="fa-brands fa-twitter-square fa-2x"></i></a>
+				<p>@iamel_g</p>
+			</li>
+			<li class="col-4 col-lg-4">
+				<a href="https://hackmd.io/@mgalvan" target="_blank"><i class="fa-solid fa-file-lines fa-2x"></i></a>
+				<p>@mgalvan</p>
+			</li>
+			<li class="col-4 col-lg-4">
+				<a href="https://gitlab.com/mgalvan-gp" target="_blank"><i class="fa-brands fa-gitlab fa-2x"></i></a>
+				<p>@m_galvan</p>
+			</li>
+		</ul>
+	</div>
 
-	<div class="text-center">
+	<div class="credits text-center">
 		<p>Melissa Galvan - 2022</p>
 	</div>
 </footer><!-- #colophon -->
 </div><!-- .site-content-contain -->
 </div><!-- #page -->
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
 
 <?php wp_footer(); ?>
 
