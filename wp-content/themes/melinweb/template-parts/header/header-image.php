f4904f1cdea686df860a046e5a8de617331062a6
m_galvan <mgalvan@outlook.fr>
2022-09-05 01:25:23 +0200

    Remove header files

--- wp-content/themes/melinweb/template-parts/header/header-image.php
+++
@@ -1,9 +0,0 @@
-<div class="custom-header <?php if(is_front_page()) : ?>banner-front-page<? endif; ?>">
-
-	<div class="custom-header-media">
-		<?php the_custom_header_markup(); ?>
-	</div>
-
-	<?php get_template_part('template-parts/header/site', 'branding'); ?>
-
-</div><!-- .custom-header -->
